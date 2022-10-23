5ff97224d6c2e80566b7a1445c1215f4fe3d3a00
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-23 15:44:13 -0400

    Update meta

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({children}) {
       <div className={`min-h-screen pb-20 ${(isDark||themeLoading)?"dark bg-gradient-to-r from-black to-slate-900":"bg-white"} px-4`}>
         <Head>
           <title>Jett Hays</title>
-          <meta name="description" content="Jett Hays is a machine learning student at CMU interested in sharing stories and building technology." />
+          <meta name="description" content="Hey, I'm Jett Hays! I make the world a better place through stories and technology. Right now, I attend Carnegie Mellon where I study statistics and machine learning." />
           <link rel="icon" href="/icon.ico" />
         </Head>
         
