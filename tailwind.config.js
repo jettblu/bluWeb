f7a771ea6571de99b8c00acca83a897c2c189b70
Jett Hays <jettblu.fabrications@gmail.com>
2022-10-28 01:13:29 -0400

    Update doc prose style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {typography: {
+      quoteless: {
+        css: {
+          'blockquote p:first-of-type::before': { content: 'none' },
+          'blockquote p:first-of-type::after': { content: 'none' },
+        },
+      },
+    },},
   },
   plugins: [require('@tailwindcss/typography'),
             plugin(function({ addUtilities }) {
