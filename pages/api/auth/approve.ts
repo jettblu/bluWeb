cde2d70352fc9f564cd2c7db2fc0316d18e1b14a
jettblu <jettblu.fabrications@gmail.com>
2023-01-21 13:55:13 -0500

    Add auth api

---
+++ pages/api/auth/approve.ts
@@ -0,0 +1,55 @@
+import { v4 } from "uuid";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { generateTokens } from "../../../src/authUtils/jwt";
+import {
+  addRefreshTokenToWhitelist,
+  findUserByEmail,
+} from "../../../prisma/script";
+import { setCookie } from "cookies-next";
+
+type Data = {
+  accessToken?: string;
+  refreshToken?: string;
+  msg?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log("running approval");
+  // Get data submitted in request's body.
+  try {
+    const body = req.body;
+    const email: string = body.email;
+    const code: string = body.code;
+    // reject if no user is supplied
+    if (!email || !code) {
+      return res.status(400).json({ msg: "Email and code are required" });
+    }
+    const user = await findUserByEmail(email);
+    // reject if user already exists
+    if (!user) {
+      return res.status(400).json({ msg: "Unable to find user" });
+    }
+    const jti = v4();
+    const { accessToken, refreshToken } = generateTokens(user, jti);
+    await addRefreshTokenToWhitelist(jti, refreshToken, user.id);
+    setCookie("accessToken", accessToken, {
+      req,
+      res,
+      secure: process.env.APP_STAGE == "production",
+      httpOnly: true,
+    });
+    setCookie("refreshToken", refreshToken, {
+      req,
+      res,
+      secure: process.env.APP_STAGE == "production",
+      httpOnly: true,
+    });
+    return res.status(200).json({ msg: "Auth cookies have been set." });
+  } catch (e: any) {
+    return res.status(400).json({ msg: `${e.message}` });
+  }
+}
