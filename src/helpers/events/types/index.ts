cc728476f31c417d7042591d6f3c1eea5bfc0152
jettblu <jettblu.fabrications@gmail.com>
2023-01-23 01:04:01 -0500

    Fix event placeholder type compatability

--- src/helpers/events/types/index.ts
+++ src/helpers/events/types/index.ts
@@ -3,7 +3,7 @@ import { BluFetch } from "../../BluFetch";
 
 export default interface EventPlaceholder {
   description: string;
-  iv?: string;
+  iv?: string | null;
   createdAt?: Date;
   userId: string;
 }
