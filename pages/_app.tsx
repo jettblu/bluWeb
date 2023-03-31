0f3974e6925c80fa7eb243110cb25d032317a71e
jettblu <jettblu.fabrications@gmail.com>
2023-03-31 14:33:34 -0400

    Update fathom router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,11 @@ import Layout from "../components/Layout";
 import { BluThemeProvider } from "../components/ThemeProvider";
 import * as Fathom from "fathom-client";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const workerRef = useRef<Worker>();
+  const router = useRouter();
 
   useEffect(() => {
     workerRef.current = new Worker(
@@ -19,6 +21,16 @@ function MyApp({ Component, pageProps }: AppProps) {
     Fathom.load("EDTJYLRZ", {
       includedDomains: ["www.jetthays.com"],
     });
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
   }, []);
 
   return (
