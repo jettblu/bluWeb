dd287a4105e45d53cf59910bf9e1a80c437d19d1
jettblu <jettblu.fabrications@gmail.com>
2023-04-09 23:02:05 -0400

    Update research page display

--- pages/research/index.tsx
+++ pages/research/index.tsx
@@ -1,80 +1,91 @@
-import { NextPage } from 'next'
-import Head from 'next/head'
+import { NextPage } from "next";
+import Head from "next/head";
 
-import Link from 'next/link'
-import ReactPlayer from 'react-player'
-import Divider from '../../components/Divider'
-
-
-
+import Link from "next/link";
+import ReactPlayer from "react-player";
+import Divider from "../../components/Divider";
 
 const Home: NextPage = () => {
-
   return (
     <div className="text-black dark:text-white">
-         <Head>
-          <title>Research</title>
-          <meta name="description" content="Research done by Jett Hays. Previous work includes a mind controlled keyboard and a digital wallet." />
-        </Head>
-        <div className="max-w-2xl mx-auto">
-            <div className="mb-12">
-                <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-sky-400 to-blue-500 mb-2 text-center">Brainstorm</h1>
-                <p className="text-slate-700 dark:text-slate-200 text-xl font-semibold text-center">Type With Your Mind</p>
-            </div>
+      <Head>
+        <title>Research</title>
+        <meta
+          name="description"
+          content="Research done by Jett Hays. Previous work includes a mind controlled keyboard and a digital wallet."
+        />
+      </Head>
+      <div className="max-w-2xl mx-auto">
+        <div className="mb-12">
+          <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-t from-sky-400 to-blue-500 mb-2 text-center">
+            Brainstorm
+          </h1>
+          <p className="text-slate-700 dark:text-slate-200 text-xl font-semibold text-center">
+            Type With Your Mind
+          </p>
         </div>
+      </div>
 
-        <div className="max-w-3xl mx-auto">
-            <div className="mb-12 mx-auto w-full max-w-[600px]">
-                <ReactPlayer url={"https://www.youtube.com/watch?v=3ONCUlBnBXE"} width="100%"/>
-            </div>  
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 gap-x-4">
-                {/* software card */}
-                <div className="bg-gray-50 dark:bg-gray-900 rounded-lg px-2 py-4 hover:shadow-md hover:shadow-sky-400 border border-gray-300 dark:border-gray-600">
-                    <h2 className="text-2xl font-semibold mb-2">Software</h2>
-                    <p className="text-lg text-green-600">An analysis of the design principles behind Brainstorm.</p>
-                    <Divider/>
-                    <div className="flex flex-row space-x-4 text-sky-400 text-lg font-bold">
-                        <Link href="../blog/brainstormSoftware">
-                            Read
-                        </Link>
-                        <a href="/brainstorm/Brainstorm Software.pdf" download>
-                           Download
-                        </a>
-                        
-                    </div>
-                </div>
-                {/* overview card */}
-                <div className="bg-gray-50 dark:bg-gray-900 rounded-lg px-2 py-4 hover:shadow-md hover:shadow-sky-400 border border-gray-300 dark:border-gray-600">
-                    <h2 className="text-2xl font-semibold mb-2">System Overview</h2>
-                    <p className="text-lg text-green-600">A high level overview of what Brainstorm is and how it works.</p>
-                    <Divider/>
-                    <div className="flex flex-row space-x-4 text-sky-400 text-lg font-bold">
-                        <Link href="../blog/brainstormOverview">
-                            Read
-                        </Link>
-                        <a href="/brainstorm/Brainstorm High Level.pdf" download>
-                           Download
-                        </a>
-                        
-                    </div>
-                </div>
-                 {/* poster card */}
-                 <div className="bg-gray-50 dark:bg-gray-900 rounded-lg px-2 py-4 hover:shadow-md hover:shadow-sky-400 border border-gray-300 dark:border-gray-600">
-                    <h2 className="text-2xl font-semibold mb-2">Research Poster</h2>
-                    <p className="text-lg text-green-600">A visual exploration of the research that resulted in Brainstorm.</p>
-                    <Divider/>
-                    <div className="flex flex-row space-x-4 text-sky-400 text-lg font-bold">
-                        <a href="/brainstorm/poster.pdf" download>
-                           Download
-                        </a>
-                        
-                    </div>
-                </div>
-            </div>
+      <div className="max-w-3xl mx-auto">
+        <div className="mb-12 mx-auto w-full max-w-[640px]">
+          <ReactPlayer
+            url={"https://www.youtube.com/watch?v=3ONCUlBnBXE"}
+            controls={true}
+            width="100%"
+            style={{
+              outline: "1px solid blue",
+              borderRadius: "5px",
+              padding: "5px",
+              backgroundColor: "skyblue",
+            }}
+          />
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 gap-x-4">
+          {/* software card */}
+          <div className="bg-gray-50 dark:bg-gray-900 rounded-lg px-2 py-4 hover:shadow-md hover:shadow-sky-400 border border-gray-300 dark:border-gray-600">
+            <h2 className="text-2xl font-semibold mb-2">Software</h2>
+            <p className="text-lg text-green-600">
+              An analysis of the design principles behind Brainstorm.
+            </p>
+            <Divider />
+            <div className="flex flex-row space-x-4 text-sky-400 text-lg font-bold">
+              <Link href="../blog/brainstormSoftware">Read</Link>
+              <a href="/brainstorm/Brainstorm Software.pdf" download>
+                Download
+              </a>
+            </div>
+          </div>
+          {/* overview card */}
+          <div className="bg-gray-50 dark:bg-gray-900 rounded-lg px-2 py-4 hover:shadow-md hover:shadow-sky-400 border border-gray-300 dark:border-gray-600">
+            <h2 className="text-2xl font-semibold mb-2">System Overview</h2>
+            <p className="text-lg text-green-600">
+              A high level overview of what Brainstorm is and how it works.
+            </p>
+            <Divider />
+            <div className="flex flex-row space-x-4 text-sky-400 text-lg font-bold">
+              <Link href="../blog/brainstormOverview">Read</Link>
+              <a href="/brainstorm/Brainstorm High Level.pdf" download>
+                Download
+              </a>
+            </div>
+          </div>
+          {/* poster card */}
+          <div className="bg-gray-50 dark:bg-gray-900 rounded-lg px-2 py-4 hover:shadow-md hover:shadow-sky-400 border border-gray-300 dark:border-gray-600">
+            <h2 className="text-2xl font-semibold mb-2">Research Poster</h2>
+            <p className="text-lg text-green-600">
+              A visual exploration of the research that resulted in Brainstorm.
+            </p>
+            <Divider />
+            <div className="flex flex-row space-x-4 text-sky-400 text-lg font-bold">
+              <a href="/brainstorm/poster.pdf" download>
+                Download
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default Home
+export default Home;
