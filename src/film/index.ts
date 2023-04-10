adccf5c3766b5def881709f62ed78c8374d03e6a
jettblu <jettblu.fabrications@gmail.com>
2023-04-09 23:20:55 -0400

    Add films

--- src/film/index.ts
+++ src/film/index.ts
@@ -1,88 +1,111 @@
 import { Film } from "./types";
 
-export const films:Film[] = [
-    {
-        title: "Wiffle Ball Hills",
-        url: "https://youtu.be/AD4qlnbHJT0",
-        thumbnailPath:"/film/thumbnails/wiffleBallHills.png",
-        description: "Playing ball in the pasture of dreams. Somewhere in magical South Dakota."
-    },
-    {
-        title: "Raider Sunday",
-        url: "https://vimeo.com/616205727",
-        thumbnailPath:"/film/thumbnails/raiderSunday.jpg",
-        description: "The Raiders and Steelers collided on a Sunday in Pittsburgh. I was in the stands to watch what turned out to be an epic Raiders victory. This was also my first time attending an NFL game."
-    },
-    {
-        title: "10 in Burgh'",
-        url: "https://vimeo.com/602944545",
-        thumbnailPath:"/film/thumbnails/burgh.jpg",
-        description: "Small moments from my first ten days in Pittsburgh."
-    },
-    {
-        title: "Concrete Surfing",
-        url: "https://vimeo.com/564493809",
-        thumbnailPath:"/film/thumbnails/concreteSurfing.png",
-        description: "Riding the electric waves with a tug rope!"
-    },
-    {
-        title: "ABD",
-        url: "https://vimeo.com/562097576",
-        thumbnailPath:"/film/thumbnails/abd.png",
-        description: "Always. Be. Dominating."
-    },
-    {
-        title: "Skydiving Before 19",
-        url: "https://www.youtube.com/watch?v=XlC0fNytLeI",
-        thumbnailPath:"/film/thumbnails/skydive.png",
-        description: "Jake and I jumped out of a plane as part of a wild birthday celebration and a (literal book) launch."
-    },
-    {
-        title: "Friday Night",
-        url: "https://vimeo.com/549745307",
-        thumbnailPath:"/film/thumbnails/fridayNight.png",
-        description: "May 14th, 2021: one (extraordinary) Friday night in the story of our lives."
-    },
-    {
-        title: "Indulge in Your Youth",
-        url: "https://vimeo.com/544306090",
-        thumbnailPath:"/film/thumbnails/gold.png",
-        description: "April 30th, 2021: a brilliant step in the dance of life."
-    },
-    {
-        title: "Dodger Baseball",
-        url: "https://vimeo.com/537595512",
-        thumbnailPath:"/film/thumbnails/dodgerGame.png",
-        description: "Reconnecting with America's favorite pastime as part of a 'sellout' crowd of 15,000. The music was bumping, the people were roaring, and the Dodgers were winning: a great night at the ballpark."
-    },
-    {
-        title: "No Alternative",
-        url: "https://vimeo.com/534104549",
-        thumbnailPath:"/film/thumbnails/noAlternative.png",
-        description: "There is no alternative to the life we live. Created during an afternoon on the cliffs with Gil: cameras included (and encouraged)."
-    },
-    {
-        title: "Palm Springin'",
-        url: "https://vimeo.com/534090106",
-        thumbnailPath:"/film/thumbnails/palmSprings.png",
-        description: "Palm Springs (and kinda sorta San Diego) with the boys for Spring Break. Wide beaches, warm pools, trunk rides, and drones included."
-    },
-    {
-        title: "Mojave Flickers",
-        url: "https://vimeo.com/534106234",
-        thumbnailPath:"/film/thumbnails/mojave.png",
-        description: "A winter journey into the Mojave."
-    },
-    {
-        title: "Forever Now",
-        url: "https://www.youtube.com/watch?v=sQBVbpHa7T8",
-        thumbnailPath:"/film/thumbnails/foreverNow.png",
-        description: "The issue with now is that it’s not forever, and the issue with forever is that it never ends. "
-    },
-    {
-        title: "Living Free",
-        url: "https://www.youtube.com/watch?v=XfEJr51iPVk",
-        thumbnailPath:"/film/thumbnails/liveFree.png",
-        description: "With school canceled because of Covid-19, we hit the road not sure of our destination, not sure of for how long, not sure of much. We ended up experiencing Malibu, Santa Barbara, and surrounding areas for next to nothing. We were up early, and up late. Just up and alive: it's great to be alive."
-    },
-]
+export const films: Film[] = [
+  {
+    title: "Weekend in Belize",
+    url: "https://www.youtu.be/I5TjGai2Lv8",
+    thumbnailPath: "/film/thumbnails/belizeWater.png",
+    description:
+      "Over 48 hours, we journeyed from the Caribbean sea to the foothills of the Mayan Mountains. My first time outside of the country!",
+  },
+  {
+    title: "Wiffle Ball Hills",
+    url: "https://youtu.be/AD4qlnbHJT0",
+    thumbnailPath: "/film/thumbnails/wiffleBallHills.png",
+    description:
+      "Playing ball in the pasture of dreams. Somewhere in magical South Dakota.",
+  },
+  {
+    title: "Becoming 20",
+    url: "https://www.youtu.be/bVxMA8tyArU",
+    thumbnailPath: "/film/thumbnails/becoming20.webp",
+    description:
+      "I called up Alexander the Great and we concocted a wild bike ride along the California Coast: 32 miles to Topanga and 30 miles through Malibu. Live fast or die slow.",
+  },
+  {
+    title: "Raider Sunday",
+    url: "https://vimeo.com/616205727",
+    thumbnailPath: "/film/thumbnails/raiderSunday.jpg",
+    description:
+      "The Raiders and Steelers collided on a Sunday in Pittsburgh. I was in the stands to watch what turned out to be an epic Raiders victory. This was also my first time attending an NFL game.",
+  },
+  {
+    title: "10 in Burgh'",
+    url: "https://vimeo.com/602944545",
+    thumbnailPath: "/film/thumbnails/burgh.jpg",
+    description: "Small moments from my first ten days in Pittsburgh.",
+  },
+  {
+    title: "Concrete Surfing",
+    url: "https://vimeo.com/564493809",
+    thumbnailPath: "/film/thumbnails/concreteSurfing.png",
+    description: "Riding the electric waves with a tug rope!",
+  },
+  {
+    title: "ABD",
+    url: "https://vimeo.com/562097576",
+    thumbnailPath: "/film/thumbnails/abd.png",
+    description: "Always. Be. Dominating.",
+  },
+  {
+    title: "Skydiving Before 19",
+    url: "https://www.youtube.com/watch?v=XlC0fNytLeI",
+    thumbnailPath: "/film/thumbnails/skydive.png",
+    description:
+      "Jake and I jumped out of a plane as part of a wild birthday celebration and a (literal book) launch.",
+  },
+  {
+    title: "Friday Night",
+    url: "https://vimeo.com/549745307",
+    thumbnailPath: "/film/thumbnails/fridayNight.png",
+    description:
+      "May 14th, 2021: one (extraordinary) Friday night in the story of our lives.",
+  },
+  {
+    title: "Indulge in Your Youth",
+    url: "https://vimeo.com/544306090",
+    thumbnailPath: "/film/thumbnails/gold.png",
+    description: "April 30th, 2021: a brilliant step in the dance of life.",
+  },
+  {
+    title: "Dodger Baseball",
+    url: "https://vimeo.com/537595512",
+    thumbnailPath: "/film/thumbnails/dodgerGame.png",
+    description:
+      "Reconnecting with America's favorite pastime as part of a 'sellout' crowd of 15,000. The music was bumping, the people were roaring, and the Dodgers were winning: a great night at the ballpark.",
+  },
+  {
+    title: "No Alternative",
+    url: "https://vimeo.com/534104549",
+    thumbnailPath: "/film/thumbnails/noAlternative.png",
+    description:
+      "There is no alternative to the life we live. Created during an afternoon on the cliffs with Gil: cameras included (and encouraged).",
+  },
+  {
+    title: "Palm Springin'",
+    url: "https://vimeo.com/534090106",
+    thumbnailPath: "/film/thumbnails/palmSprings.png",
+    description:
+      "Palm Springs (and kinda sorta San Diego) with the boys for Spring Break. Wide beaches, warm pools, trunk rides, and drones included.",
+  },
+  {
+    title: "Mojave Flickers",
+    url: "https://vimeo.com/534106234",
+    thumbnailPath: "/film/thumbnails/mojave.png",
+    description: "A winter journey into the Mojave.",
+  },
+  {
+    title: "Forever Now",
+    url: "https://www.youtube.com/watch?v=sQBVbpHa7T8",
+    thumbnailPath: "/film/thumbnails/foreverNow.png",
+    description:
+      "The issue with now is that it’s not forever, and the issue with forever is that it never ends. ",
+  },
+  {
+    title: "Living Free",
+    url: "https://www.youtube.com/watch?v=XfEJr51iPVk",
+    thumbnailPath: "/film/thumbnails/liveFree.png",
+    description:
+      "With school canceled because of Covid-19, we hit the road not sure of our destination, not sure of for how long, not sure of much. We ended up experiencing Malibu, Santa Barbara, and surrounding areas for next to nothing. We were up early, and up late. Just up and alive: it's great to be alive.",
+  },
+];
