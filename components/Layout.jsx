b3a6468221a7dc1468a74ca7979382ffec2f3b07
jettblu <jettblu.fabrications@gmail.com>
2023-04-20 17:34:59 -0400

    Update default theme detection

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -14,9 +14,7 @@ export default function Layout({ children }) {
   return (
     <div
       className={`min-h-screen pb-20 ${
-        isDark || themeLoading
-          ? "dark bg-gradient-to-r from-black to-slate-900"
-          : "bg-white"
+        isDark ? "dark bg-gradient-to-r from-black to-slate-900" : "bg-white"
       } px-4`}
     >
       <Head>
--- src/bluThemeHelper.ts
+++ src/bluThemeHelper.ts
@@ -1,72 +1,76 @@
-
 import { useEffect, useState } from "react";
 
-
 export function useBluTheme() {
-    // init state
-    const [isDark, setIsDark] = useState(false);
-    const [themeLoading, setThemeLoading] = useState(true);
-    
-    const defaultTheme:ITheme = {
-        isDark: true,
-        lastUpdated: Date.now()
-    }
-    interface ITheme{
-        isDark:boolean,
-        lastUpdated: number
-    }
+  // init state
+  const [isDark, setIsDark] = useState(false);
+  const [themeLoading, setThemeLoading] = useState(true);
 
-    const generateThemeLocation = function(){
-        let themeLocation = `blu|theme`
-        return themeLocation;
+  const defaultTheme: ITheme = {
+    isDark: true,
+    lastUpdated: Date.now(),
+  };
+  interface ITheme {
+    isDark: boolean;
+    lastUpdated: number;
+  }
+
+  const generateThemeLocation = function () {
+    let themeLocation = `blu|theme`;
+    return themeLocation;
+  };
+
+  const fetchTheme = function () {
+    let theme: ITheme;
+    let themeString: string | null = null;
+    const themeLocation = generateThemeLocation();
+    themeString = localStorage.getItem(themeLocation);
+    // fetch stored theme
+    if (!themeString) {
+      theme = defaultTheme;
+    } else {
+      theme = { ...JSON.parse(themeString) };
     }
+    // update state
+    setIsDark(theme.isDark);
+  };
 
-    const fetchTheme = function(){
-        let theme:ITheme;
-        let themeString:string|null = null;
-        const themeLocation = generateThemeLocation()
-        themeString = localStorage.getItem(themeLocation);
-        // fetch stored theme
-        if(!themeString){
-            theme = defaultTheme
-        }
-        else{
-            theme = {...JSON.parse(themeString)}
-        }
-        // update state
-        setIsDark(theme.isDark);
+  function updateIsDark(newIsDark: boolean) {
+    console.log("Updating is dark...");
+    let newTheme: ITheme = {
+      isDark: newIsDark,
+      lastUpdated: Date.now(),
+    };
+    // update app state
+    setIsDark(newIsDark);
+    // update stored theme
+    updateTheme(newTheme);
+  }
+
+  // updates local storage theme value
+  const updateTheme = function (newTheme: ITheme) {
+    let themeLocation = generateThemeLocation();
+    let themeString = JSON.stringify(newTheme);
+    localStorage.setItem(themeLocation, themeString);
+  };
+
+  useEffect(() => {
+    setThemeLoading(true);
+    // fetch current theme
+    // fetchTheme();
+    if (window && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      console.log("Dark mode detected");
+      setIsDark(true);
+    } else {
+      console.log("Light mode detected");
+      setIsDark(false);
     }
+    console.log("Theme loaded.");
+    setThemeLoading(false);
+  }, []);
 
-    function updateIsDark(newIsDark:boolean){
-        console.log("updating is dark...");
-        let newTheme:ITheme = {
-            isDark: newIsDark,
-            lastUpdated: Date.now()
-        }
-        // update app state
-        setIsDark(newIsDark);
-        // update stored theme
-        updateTheme(newTheme);
-    }
-
-
-    // updates local storage theme value
-    const updateTheme = function(newTheme:ITheme){
-        let themeLocation = generateThemeLocation();
-        let themeString = JSON.stringify(newTheme);
-        localStorage.setItem(themeLocation, themeString);
-    }
-
-    useEffect(()=>{
-        setThemeLoading(true);
-        // fetch current theme
-        fetchTheme();
-        setThemeLoading(false);
-    }, [])
-
-    return{
-        isDark, 
-        updateIsDark,
-        themeLoading
-    }
+  return {
+    isDark,
+    updateIsDark,
+    themeLoading,
+  };
 }
