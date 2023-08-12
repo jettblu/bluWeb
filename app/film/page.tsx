085c3ee60264a7f1c70762752be92a373ef20156
jettblu <jettblu.fabrications@gmail.com>
2023-08-11 22:50:12 -0700

    Add film page to app router

--- app/film/page.tsx
+++ app/film/page.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
 import Head from "next/head";
 import { films } from "../../src/film";
 import { Film } from "../../src/film/types";
