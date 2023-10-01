8e047f2f260affdd67b09336c4aadbb533f765fd
jettblu <jettblu.fabrications@gmail.com>
2023-10-01 16:41:44 -0400

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,17 @@
 import React from "react";
 import ActivitiesSection from "../components/activities/ActivitiesSection";
-import SplashMain from "../components/splash/SplashMain";
+import ProfileCard from "../components/profile/ProfileCard";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <div className="h-[10vh]" />
-      <div className="bluFont z-10 text-left bg-sky-400/10 md:bg-sky-400/70 md:ring-1 ring-green-400 w-fit p-2 rounded-md backdrop-blur-xl mx-auto">
-        <h1 className="text-8xl">
-          Hey, I'm <span className="text-green-400">Jett Hays</span>
-        </h1>
-        <p className="text-4xl">
-          I am an explorer. I visit the unvisited and learn the unknown.
-        </p>
-      </div>
-      <SplashMain />
-      {/* <div className="mx-auto max-w-2xl w-full">
+      <div className="mx-auto max-w-2xl w-full">
         <div className="mb-12">
           <ProfileCard />
         </div>
         <div className="w-[100%] h-[2px] bg-gradient-to-r from-green-400 to-blue-500" />
         <ActivitiesSection />
-      </div> */}
-      <div className="h-[80vh]" />
-      <ActivitiesSection />
+      </div>
     </main>
   );
 }
